b6d799d4cd176d4abb5417a8ad1efb2c09675208
stinkyfingers <john_shenk@hotmail.com>
2014-06-02 04:51:15 -0500

    adds basic html page

---
+++ index.php
@@ -0,0 +1,40 @@
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Prime Numbers</title>
+         <script src="http://code.jquery.com/jquery-1.11.0.min.js"></script>
+    </head>
+    <body>
+        <section class="main">
+
+            <h3>Prime Numbers</h3>
+            <button id="primeNumbers">Get Prime Numbers</button>
+
+            <div id="output">Output: </div>
+
+
+        </section>
+
+    </body>
+
+
+    <script>
+        $("#primeNumbers").click(function(){
+            getPrimes();
+        });
+        
+        var getPrimes = function(){
+
+            $.post("primes.php",function(response){
+                console.log(response);
+                alert(response);
+                $("#output").html(response);
+                },'json');
+
+         }; 
+            
+        
+
+
+    </script>
+</html>
