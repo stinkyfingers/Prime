64faef87085eb027ce63944b860d947385c3e5b9
stinkyfingers <john_shenk@hotmail.com>
2014-06-02 06:08:30 -0500

    Completes getPrimesArray method, tests ok

--- PrimesClass.php
+++ PrimesClass.php
@@ -6,6 +6,33 @@ class PrimesClass{
         
     }
     
+    public function getPrimeArray($startingNumber, $quantity){
+        $result = array();
+        $primeTest = true;
+        
+        while(count($result) < $quantity){
+            //prime test logic
+            for($i=2;$i<$startingNumber;$i++){
+                if($startingNumber%$i==0){
+                    $primeTest=false;
+                }
+            }
+            
+            //prime? append to array
+            if($primeTest){
+                array_push($result,$startingNumber);
+            }
+            $startingNumber++;
+            $primeTest = true;
+        }
+
+        return $result;
+ 
+    }
+
+    
+    
+    
     
 }
 
