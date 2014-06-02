36c502d1e7a8ae634189b42929cdc55130190ea7
stinkyfingers <john_shenk@hotmail.com>
2014-06-02 06:31:48 -0500

    adds basic table styling

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
         <meta charset="UTF-8">
         <title>Prime Numbers</title>
         <script src="http://code.jquery.com/jquery-1.11.0.min.js"></script>
+        <link rel="stylesheet" href="main.css">
     </head>
     <body>
         <section class="main">
@@ -28,13 +29,12 @@
                 quantity: '1000'
             }
             $.post("primes.php",data,function(response){
-                $("#output").append("<table><tr><th>Index</th><th>Value</th></tr>");
+                $("#output").append("<table id='primeTable'><tr><th>Index</th><th>Value</th></tr></table>");
                 $.each(response, function(index,el){
                     //alert(index+"is: "+el);
-                    $("#output").append("<tr><td>"+(index+1)+"</td><td>"+el+"</td></tr>");
+                    $("#primeTable").append("<tr><td>"+(index+1)+"</td><td>"+el+"</td></tr>");
                     
                 });
-                $("#output").append("<table>");
                 },'json');
 
          }; 
